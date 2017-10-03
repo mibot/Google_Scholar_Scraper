a93fbeb10558fac7887aedd1d25940e9896dcb0d
Miguel Botto Tobar <mabt08@gmail.com>
2017-10-03 12:59:54 +0200

    last changes

--- GoogleScholarScraper.r
+++ GoogleScholarScraper.r
@@ -6,25 +6,24 @@
 # For more information see: http://creativecommons.org/licenses/by-nc/4.0/
 # All rights reserved.
 
-GScholar_Scraper <- function(input, since = 1900, write = F){
+GScholar_Scraper <- function(input, page.ini = 1, pages.max = 0, since = 1900, to = 2017, write = F){
   require(httr)
   require(XML)
   require(RCurl)
   
   # putting together the search-URL:
-  URL <- paste0("http://scholar.google.com/scholar?q=",input,"&btnG=&hl=en&as_sdt=1&as_vis=1&as_ylo=",since)
+  URL <- paste0("http://scholar.google.com/scholar?q=",input,"&hl=en&as_sdt=1,5&as_vis=1&as_ylo=",since,"&as_yhi=",to)
   
-  cat("\nThe URL used is: ", "\n----\n", paste0("* ","http://scholar.google.com/scholar?q=",input,"&btnG=&hl=en&as_sdt=1&as_vis=1&as_ylo=", since, " *"))
+  cat("\nThe URL used is: ", "\n----\n", paste0("* ","http://scholar.google.com/scholar?q=",input,"&hl=en&as_sdt=1,5&as_vis=1&as_ylo=", since, "&as_yhi=",to," *"))
   
   # get content and parse it:
-  #html <- getURL(URL, .encoding = "CE_UTF8")
   doc <- htmlParse(URL)
   
   # number of hits:
   h1 <- xpathSApply(doc, "//div[@id='gs_ab_md']", xmlValue)
   
   h2 <- unlist(strsplit(h1, "\\s"))
-  
+
   # in splitted string it is the second element which contains digits,
   # grab it and remove decimal signs and convert to integer
   num <- as.integer(gsub("[[:punct:]]", "", h2[grep("\\d", h2)[1]]))
@@ -34,15 +33,18 @@ GScholar_Scraper <- function(input, since = 1900, write = F){
     stop("\n\n...There is no result for the submitted search string!")
   }
   
-  pages.max <- ceiling(num/10)
-  #print(pages.max)
-  # 'start' as used in URL:
-  start <- 10 * 1:pages.max - 10
-  #print(start)
-  # Collect URLs as list:
-  URLs <- paste("http://scholar.google.com/scholar?start=",start,"&q=",input, 
-                "&btnG=&hl=en&as_sdt=1&as_vis=1&as_ylo=",since,sep="")
+  #if (page.ini == 0 | is.na(page.ini)) page.ini <- 1
+  if (pages.max == 0 | is.na(pages.max)) pages.max <- ceiling(num/10)
   
+  # 'start' as used in URL:
+  start <- 10 * page.ini:pages.max - 10
+  
+  
+  # Collect URLs as list:
+  #URLs <- paste("http://scholar.google.com/scholar?start=",start,"&q=",input, 
+  #              "&btnG=&hl=en&as_sdt=1&as_vis=1&as_ylo=",since,sep="")
+  URLs <- paste("http://scholar.google.com/scholar?start=",start,"&q=",input, 
+                "&hl=en&as_sdt=1,5&as_vis=1&as_ylo=",since,"&as_yhi=",to,sep="")
   
   GS_xpathSApply <- function(doc, path, FUN) {
     path.base <- "//div[@class='gs_r']"
@@ -57,55 +59,124 @@ GScholar_Scraper <- function(input, since = 1900, write = F){
     } else {
       xx <- sapply(paths, function(x) xpathSApply(doc, x, FUN), USE.NAMES = FALSE)
     }
+    xx <- sapply(1:length(xx), function(x) xx[[x]][1])
     
     # convert NULL to NA in list
     xx[sapply(xx, length) < 1] <- NA
     
     # return node values as a vector
     xx <- as.vector(unlist(xx))
+    
     return(xx)
   }
   
   scraper_internal <- function(URL){
-    doc <- htmlParse(URL)
+    print(URL)
+    
+    doc <- htmlParse(URL, encoding="UTF-8")
     
     #titles:
-    title <- GS_xpathSApply(doc, "//h3", xmlValue)
+    title <- GS_xpathSApply(doc, "//div[@class='gs_ri']//h3[@class='gs_rt']", xmlValue)
     
     #types:
-    type = GS_xpathSApply(doc, "//h3//span//span[@class='gs_ct1']", xmlValue)
+    #type = GS_xpathSApply(doc, "//h3//span//span[@class='gs_ct1']", xmlValue)
     
     #authors:
-    author = GS_xpathSApply(doc, "//div[@class='gs_ri']//div[@class='gs_a']", xmlValue)
+    author <-  GS_xpathSApply(doc, "//div[@class='gs_ri']//div[@class='gs_a']", xmlValue)
+    
+    #source:
+    source  <-  GS_xpathSApply(doc, "//div[@class='gs_ggs gs_fl']//a[@href]", xmlValue)
+    #print(source)
+    #m.url <-  xpathSApply(doc, "//div[@class='gs_r']//div[@class='gs_ri']//h3[@class='gs_rt']", function(x) ifelse(is.null(xmlChildren(x)$a), NA, xmlAttrs(xmlChildren(x)$a, 'href')))
+    #print(m.url) function(x) ifelse(x, NA, xmlGetAttr(x, 'href'))
+    #o.url <- xpathSApply(doc, "//div[@class='gs_fl']//a[@class='gs_nph'][contains(., 'versions')]", function(x) ifelse(is.null(x), NA, xmlGetAttr(x, 'href')))
+    #print(o.url)
+    #d.url <-  xpathSApply(doc, "//div[@class='gs_r']//div[@class='gs_ggs gs_fl']", xmlValue)
     
     #links:
-    main_url = GS_xpathSApply(doc,  "//h3//a[@href]", xmlGetAttr)
-    direct_url = GS_xpathSApply(doc,  "//div[@class='gs_ggs gs_fl']//a[@href]", xmlGetAttr)
+    mainLink <-  GS_xpathSApply(doc,  "//h3//a[@href]", xmlGetAttr)
+    #print(mainLink)
+    otherVersionLink <- GS_xpathSApply(doc, "//div[@class='gs_ri']//div[@class='gs_fl']//a[@class='gs_nph'][contains(., 'versions')]", xmlGetAttr)
+    
+    directLink <-  GS_xpathSApply(doc,  "//div[@class='gs_ggs gs_fl']//a[@href]", xmlGetAttr)
+    #print(directLink)
+    
+    #cited by:
+    cited.by = GS_xpathSApply(doc, "//div[@class='gs_ri']//div[@class='gs_fl']//a[contains(.,'Cited by')]/text()", xmlValue)
+    
+    #cited ref:
+    #cited.ref = GS_xpathSApply(doc, "/div[@class='gs_ri']//div[@class='gs_fl']//a[contains(.,'Cited by')][@href]", xmlAttrs)
+    
+    
+    #print(direct_url)
+    
+    #types:
+    #type = GS_xpathSApply(doc, "//div[@class='gs_ggs gs_fl']//span[@class='gs_ctg2']", xmlValue)
     
     options(warn=(-1))
-    dat <- data.frame(TITLE = title, 
-                      TYPE = type, 
-                      AUTHORS = author,
-                      YEAR = as.integer(gsub(".*\\s(\\d{4})\\s.*", "\\1", author)), 
-                      MAIN_LINK = main_url,
-                      DIRECT_LINK = direct_url,
+    dat <- data.frame(source = source,
+                      title = title, 
+                      #TYPE = type, 
+                      authors = author,
+                      year = as.integer(gsub(".*\\s(\\d{4})\\s.*", "\\1", author)), 
+                      ncites = as.integer(as.integer(gsub("[^0-9]", "", cited.by))), 
+                      #CITED_REF = cited.ref,
+                      mainLink = mainLink,
+                      otherVersionLink = otherVersionLink,
+                      directLink = directLink,
                       stringsAsFactors = FALSE)
     
     options(warn=0)
+    dat$source <- gsub(" ", "", gsub(".*\\]", "", dat$source))
+    dat$title <- gsub(".*\\]", "", dat$title)
+    #dat$otherVersionLink <- gsub("#", NA, dat$otherVersionLink)
+    
+    #to be used in Rascal
+    dat$source <- sapply(dat$source, function(x) ifelse (is.na(x), NA, paste("|http://",x,"|", sep = "")))
+    dat$mainLink <- sapply(dat$mainLink, function(x) ifelse (is.na(x), NA, paste("|",x,"|", sep = "")))
+    dat$otherVersionLink <- sapply(dat$otherVersionLink, function(x) ifelse (is.na(x), NA, paste("|https://scholar.google.com",x,"|", sep = "")))
+    dat$directLink <- sapply(dat$directLink, function(x) ifelse (is.na(x), NA, paste("|",x,"|", sep = "")))
+    
+    
     free(doc)
-    dat$TITLE <- sub(".*\\] ", "", dat$TITLE)
-    dat$TYPE <- gsub("\\]", "", gsub("\\[", "", dat$TYPE))
     return(dat)
   }
   
   result <- do.call("rbind", lapply(URLs, scraper_internal))
   if (write == T) {
-    write.csv(result, "/Users/mbotto/Google_Scholar_Scraper/GScholar_Output.CSV",
+    con <- file("/Users/mbotto/Google_Scholar_Scraper/GScholar_Output.CSV", encoding = "UTF-8")
+    write.csv(result, con,
               row.names = F)
   } 
   return(result)
   
 }
-input <- "allintitle:live on mars"
-df <- GScholar_Scraper(input, since = 2008, write = T)
-print(xtable(df), type="html", file="/Users/mbotto/Google_Scholar_Scraper/GScholar_Output.html")
+
+input <- '"crosslanguage" +copy +detection'
+df <- GScholar_Scraper(input, 1, 0, since = 2008, to = 2017)
+
+
+
+df <- cbind(id = as.numeric(rownames(df)), downloaded = as.character(FALSE), df)
+df_save <- df[c(-5:-7)]
+con <- file("/Users/mbotto/Google_Scholar_Scraper/ProgressData.csv", encoding = "UTF-8")
+write.csv(df_save, con, row.names = F, na = "")
+
+
+##check url
+readUrl <- function(url) {
+  out <- tryCatch(
+    {
+      HEAD(url)
+    },
+    error = function(cond) {
+      return(NA)
+    },
+    warning = function(cond) {
+      return(NA)
+    },
+    finally = { 
+    }
+  )    
+  return(out)
+}
